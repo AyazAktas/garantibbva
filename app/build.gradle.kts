e8eccefac1215791a0247a629820941f2f4fa562
AyazAktaş <ayazaktastr@gmail.com>
2024-08-28 09:46:36 +0300

    login screen and navigation host

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -45,8 +46,9 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation("androidx.fragment:fragment-ktx:1.7.0")
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/garantibbva/ui/fragment/AnasayfaFragment.kt
+++ app/src/main/java/com/example/garantibbva/ui/fragment/AnasayfaFragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.garantibbva.R
 import com.example.garantibbva.databinding.FragmentAnasayfaBinding
 
 class AnasayfaFragment : Fragment() {
     private lateinit var binding: FragmentAnasayfaBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding=FragmentAnasayfaBinding.inflate(inflater,container,false)
+        binding = FragmentAnasayfaBinding.inflate(inflater, container, false)
+
+        binding.button3.setOnClickListener {
+            findNavController().navigate(R.id.action_anasayfaFragment_to_loginFragment)
+        }
+
         return binding.root
     }
 }
